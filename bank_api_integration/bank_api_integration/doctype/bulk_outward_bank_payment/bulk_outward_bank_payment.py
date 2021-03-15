5258ef5f6d5370ae66b1d0b6a566d56be450cdf1
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-03-15 08:16:32 +0530

    show total payment and no of payments in bulk outward payment

--- bank_api_integration/bank_api_integration/doctype/bulk_outward_bank_payment/bulk_outward_bank_payment.py
+++ bank_api_integration/bank_api_integration/doctype/bulk_outward_bank_payment/bulk_outward_bank_payment.py
@@ -5,8 +5,16 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 class BulkOutwardBankPayment(Document):
+	def validate(self):
+		total_payment = 0
+		for row in self.outward_bank_payment_details:
+			total_payment+=flt(row.amount)
+		self.total_payment = total_payment
+		self.no_of_payments = len(self.outward_bank_payment_details)
+
 	def on_submit(self):
 		if self.status == 'Approved':
 			for row in self.outward_bank_payment_details:
@@ -14,6 +22,7 @@ class BulkOutwardBankPayment(Document):
 				'party_type': row.party_type,
 				'party': row.party,
 				'amount': row.amount,
+				'reconcile_action': self.reconcile_action,
 				'bobp': self.name})
 				doc.save()
 				doc.submit()
