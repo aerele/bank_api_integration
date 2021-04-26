66be5117be3e82a001dc9756b6920238c40021b4
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-04-26 16:43:36 +0530

    payment entry creation, update transaction, init txn functions updated

--- bank_api_integration/bank_api_integration/doctype/outward_bank_payment/outward_bank_payment.py
+++ bank_api_integration/bank_api_integration/doctype/outward_bank_payment/outward_bank_payment.py
@@ -14,63 +14,100 @@ from frappe.utils import get_site_path
 from erpnext.accounts.doctype.payment_entry.payment_entry import get_negative_outstanding_invoices, get_orders_to_be_billed
 from erpnext.controllers.accounts_controller import get_supplier_block_status
 from erpnext.accounts.utils import get_outstanding_invoices, get_account_currency
-
+from frappe.utils import add_months, nowdate
 class OutwardBankPayment(Document):
-	# def on_change(self):
-	# 	if self.reconcile_action == 'Auto Reconcile Oldest First Invoice' and self.status == 'Transaction Completed':
-	# 		self.create_payment_entry()
+	def on_change(self):
+		if self.reconcile_action == 'Auto Reconcile Oldest First Invoice' and self.status == 'Transaction Completed':
+			references = []
+			amount = self.amount
+			month_threshold = -6
+			from_date = add_months(nowdate(), month_threshold)
+			invoices = frappe.db.get_all('Purchase Invoice',{'supplier': self.party, 'posting_date': ['>=', from_date], 'posting_date': ['<=', nowdate()]}, ['grand_total', 'due_date', 'bill_no', 'name'])
+			for inv in invoices:
+				if inv['grand_total'] <= amount:
+					references.append({
+					'reference_doctype': 'Purchase Invoice',
+					'reference_name': inv['name'],
+					'bill_no': inv['bill_no'],
+					'due_date': inv['due_date'],
+					'total_amount': inv['grand_total']
+					})
+					amount-= inv['grand_total']
+			self.create_payment_entry(references)
+		if self.reconcile_action == 'Manual Reconcile' and self.status == 'Transaction Completed':
+			references = []
+			for row in self.payment_references:
+				references.append({
+				'reference_doctype': row.reference_doctype,
+				'reference_name': row.reference_name,
+				'bill_no': row.bill_no,
+				'due_date': row.due_date,
+				'total_amount': row.total_amount,
+				'outstanding_amount': row.outstanding_amount,
+				'allocated_amount': row.allocated_amount,
+				'exchange_rate': row.exchange_rate
+				})
+			self.create_payment_entry(references)
 
-	# def create_payment_entry(self):
-	# 	payment_entry_dict = {
-	# 		"company" : self.company,
-	# 		"payment_type" : 'Pay',
-	# 		"mode_of_payment": 'Wire Transfer',
-	# 		"party_type" :  self.party_type,
-	# 		"party" :  self.party,
-	# 		"posting_date" : today(),
-	# 		"paid_amount": self.amount
-	# 	}
-	# 	payment_entry = frappe.new_doc("Payment Entry")
-	# 	payment_entry.update(payment_entry_dict)
+	def create_payment_entry(self, references):
+		payment_entry_dict = {
+			"company" : self.company,
+			"payment_type" : 'Pay',
+			"mode_of_payment": 'Wire Transfer',
+			"party_type" :  self.party_type,
+			"party" :  self.party,
+			"posting_date" : today(),
+			"paid_amount": self.amount,
+			"references": references
+		}
+		payment_entry = frappe.new_doc("Payment Entry")
+		payment_entry.update(payment_entry_dict)
 
-	# 	payment_entry.insert()
-	# 	payment_entry.submit()
+		payment_entry.insert()
+		payment_entry.submit()
 
-	# 	return payment_entry.name
+		self.payment_entry = payment_entry.name
+		self.save()
+		self.reload()
 
 	def on_submit(self):
 		if self.workflow_state == 'Approved':
 			res = None
 			currency = frappe.db.get_value("Company", self.company, "default_currency")
 			prov, config = self.get_api_provider_class()
-			print(prov)
 			integration_doc = frappe.get_doc('Bank API Integration', {'bank_account': self.company_bank_account})
 			filters = {
 				"UNIQUEID": self.name,
-				"IFSC": integration_doc.ifsc,
+				"IFSC": frappe.db.get_value('Bank Account', 
+						{'party_type': self.party_type,
+						'party': self.party,
+						'is_default': 1
+						},'ifsc_code'),
 				"AMOUNT": self.amount,
 				"CURRENCY": currency,
 				"TXNTYPE": self.transaction_type,
 				"PAYEENAME": self.party,
 				"DEBITACC": integration_doc.account_number,
-				"CREDITACC": "000405002777",
+				"CREDITACC": frappe.db.get_value('Bank Account', 
+						{'party_type': self.party_type,
+						'party': self.party,
+						'is_default': 1
+						},'bank_account_no'),
 				"WORKFLOW_REQD": "N"
 
 			}
 			try:
 				res = prov.initiate_transaction_without_otp(filters)
-				res['status'] = 'Error'
-				if res['status'] == 'Success':
-					self.status = 'Initiated'
-					self.workflow_state = 'Initiated'
-				if res['status'] == 'Failed':
-					self.status = 'Initiation Failed'
-					self.workflow_state = 'Initiation Failed'
-					self.error_message = f'Initiate Transaction API Failed: {msg}'
-				if res['status'] == 'Error':
+				if type(res) == str:
 					self.status = 'Initiation Error'
 					self.workflow_state = 'Initiation Error'
-					self.error_message = f'Initiate Transaction API Error: {msg}'
+				else:
+					if res['STATUS'] == 'SUCCESS':
+						self.status = 'Initiated'
+						self.workflow_state = 'Initiated'
+					if res['STATUS'] == 'FAILURE':
+						self.status = 'Initiation Failed'
+						self.workflow_state = 'Initiation Failed'
 			except:
 				self.status = 'Initiation Error'
 				self.workflow_state = 'Initiation Error'
@@ -78,9 +115,9 @@ class OutwardBankPayment(Document):
 			log_name = self.log_request('Initiate Transaction without OTP', filters, config, res)
 			self.save()
 			self.reload()
-			if self.status == 'Initiation Error':
-				frappe.throw(_(f'Kindly check request log for more info {log_name}'))
-
+			if self.status in ['Initiation Error', 'Initiation Failed']:
+				if not self.bobp:
+					frappe.throw(_(f'Kindly check request log for more info {log_name}'))
 
 	def get_api_provider_class(self):
 		integration_doc = frappe.get_doc('Bank API Integration', {'bank_account': self.company_bank_account})
@@ -112,165 +149,124 @@ class OutwardBankPayment(Document):
 		frappe.db.commit()
 		return request_log.name
 
-# def initiate_transaction(provider_name, config_info, use_sandbox, proxy_dict, file_paths):
-# 	prov = CommonProvider(provider_name, config_info, use_sandbox, proxy_dict, file_paths)
-# 	res = prov.initiate_transaction({})
-# 	print(res)
-# 	return res
+@frappe.whitelist()
+def update_transaction_status(obp_name=None,bobp_name=None):
+	if obp_name:
+		obp_list = {'name': obp_name}
+	if bobp_name:
+		obp_list = frappe.db.get_all('Outward Bank Payment', {'status': ['=', 'Initiated'], 'bobp': ['=', bobp_name]})
 
-# def update_transaction_status():
-# 	settings  = frappe.get_single('Bank API Integration Settings')
-# 	config_info = {'merchant_id': settings.merchant_id}
-# 	prov = CommonProvider(settings.bank_api_provider, config_info)
-# 	obp_list = frappe.db.get_all('Outward Bank Payment', {'status': ['in', ['Initiated','Transaction Error', 'Transaction Pending']]})
-# 	for doc in obp_list:
-# 		obp_doc = frappe.get_doc('Outward Bank Payment', doc['name'])
-# 		if obp_doc.status == 'Initiated':
-# 			obp_doc.retry_count = 0
-# 			obp_doc.error_message = None
-# 		else:
-# 			obp_doc.retry_count +=1
-# 		try:
-# 			config_info['order_id'] = obp_doc.name
-# 			res = prov.get_transaction_status()
-# 			msg = res['msg']
-# 			if res['status'] == 'Success':
-# 				obp_doc.status = 'Transaction Completed'
-# 				obp_doc.workflow_state = 'Transaction Completed'
-# 			if res['status'] == 'Failed':
-# 				obp_doc.status = 'Transaction Failed'
-# 				obp_doc.workflow_state = 'Transaction Failed'
-# 				obp_doc.error_message = f'Fetch Transaction Status API Failed: {msg}'
-# 			if res['status'] == 'Pending':
-# 				obp_doc.status = 'Transaction Pending'
-# 				obp_doc.workflow_state = 'Transaction Pending'
-# 				obp_doc.error_message = f'Transaction Pending: {msg}'			
-# 		except:
-# 			obp_doc.status = 'Transaction Error'
-# 			obp_doc.workflow_state = 'Transaction Error'
-# 			obp_doc.error_message = 'Fetch Transaction Status API Error: '+ frappe.get_traceback()
-# 		obp_doc.save()
+	for doc in obp_list:
+		res = None
+		obp_doc = frappe.get_doc('Outward Bank Payment', doc['name'])
+		currency = frappe.db.get_value("Company", obp_doc.company, "default_currency")
+		prov, config = obp_doc.get_api_provider_class()
+		integration_doc = frappe.get_doc('Bank API Integration', {'bank_account': obp_doc.company_bank_account})
+		filters = {"UNIQUEID": obp_doc.name}
+		try:
+			res = prov.get_transaction_status(filters)
+			if type(res) == str:
+				obp_doc.status = 'Transaction Error'
+				obp_doc.workflow_state = 'Transaction Error'
+			else:
+				if res['STATUS'] == 'SUCCESS':
+					obp_doc.status = 'Transaction Completed'
+					obp_doc.workflow_state = 'Transaction Completed'
+				if res['STATUS'] in ['FAILURE', 'DUPLICATE']:
+					obp_doc.status = 'Transaction Failed'
+					obp_doc.workflow_state = 'Transaction Failed'
+				if res['STATUS'] in ['PENDING', 'Pending For Processing']:
+					obp_doc.status = 'Transaction Pending'
+					obp_doc.workflow_state = 'Transaction Pending'
+		except:
+			obp_doc.status = 'Transaction Error'
+			obp_doc.workflow_state = 'Transaction Error'
+			res = frappe.get_traceback()
+		
+		log_name = obp_doc.log_request('Update Transaction Status', filters, config, res)
+		obp_doc.save()
+		obp_doc.reload()
+		if obp_doc.status in ['Transaction Pending', 'Transaction Error', 'Transaction Failed']:
+			if not obp_doc.bobp:
+				frappe.throw(_(f'Kindly check request log for more info {log_name}'))
 
-# def process_uninitiated_transaction():
-# 	obp_list = frappe.db.get_all('Outward Bank Payment', {'status': 'Initiation Error','retry_count': ('<', 5)})
-# 	for doc in obp_list:
-# 		obp_doc = frappe.get_doc('Outward Bank Payment', doc['name'])
-# 		currency = frappe.db.get_value("Company", obp_doc.company, "default_currency")
-# 		try:
-# 			config_info = {'order_id': obp_doc.name, 'user_info':{'cust_id':'XXX'}, 'txn_info':{'amount': obp_doc.amount,'currency': currency}}
-# 			obp_doc.retry_count += 1
-# 			res = initiate_transaction(config_info)
-# 			msg = res['msg']
-# 			if res['status'] == 'Success':
-# 				obp_doc.status = 'Initiated'
-# 				obp_doc.workflow_state = 'Initiated'
-# 			if res['status'] == 'Failed':
-# 				obp_doc.status = 'Initiation Failed'
-# 				obp_doc.workflow_state = 'Initiation Failed'
-# 				obp_doc.error_message = f'Initiate Transaction API Failed: {msg}'
-# 			if res['status'] == 'Error':
-# 				obp_doc.status = 'Initiation Error'
-# 				obp_doc.workflow_state = 'Initiation Error'
-# 				obp_doc.error_message = f'Initiate Transaction API Error: {msg}'
-# 		except:
-# 			obp_doc.status = 'Initiation Error'
-# 			obp_doc.workflow_state = 'Initiation Error'
-# 			obp_doc.error_message = 'Initiate Transaction API Error: '+frappe.get_traceback()
-# 		obp_doc.save()
+@frappe.whitelist()
+def get_outstanding_reference_documents(args):
 
-def initiate_transaction(provider_name, config_info, use_sandbox, proxy_dict, file_paths):
-	prov = CommonProvider(provider_name, config_info, use_sandbox, proxy_dict, file_paths)
-	res = prov.initiate_transaction({})
-	return res
+	if isinstance(args, string_types):
+		args = json.loads(args)
 
-# def create_workflow_state():
-# 	states = {'Initiated': 'Success', 'Initiation Error': 'Danger', 'Initiation Failed': 'Danger',
-# 		'Transaction Error': 'Danger','Transaction Failed': 'Danger', 'Transaction Pending': 'Primary', 'Transaction Completed': 'Success'}
-# 	for state in states.keys():
-# 		if not frappe.db.exists("Workflow State", state):
-# 			workflow_state=frappe.new_doc("Workflow State")
-# 			workflow_state.style= states[state]
-# 			workflow_state.save()
+	if args.get('party_type') == 'Member':
+		return
 
-# @frappe.whitelist()
-# def get_outstanding_reference_documents(args):
+	args['party_account'] =  frappe.db.get_value('Account', {'account_type': 'Payable','is_group': 0, 'company': args.get('company')})
 
-# 	if isinstance(args, string_types):
-# 		args = json.loads(args)
+	# confirm that Supplier is not blocked
+	if args.get('party_type') == 'Supplier':
+		supplier_status = get_supplier_block_status(args['party'])
+		if supplier_status['on_hold']:
+			if supplier_status['hold_type'] == 'All':
+				return []
+			elif supplier_status['hold_type'] == 'Payments':
+				if not supplier_status['release_date'] or getdate(nowdate()) <= supplier_status['release_date']:
+					return []
 
-# 	if args.get('party_type') == 'Member':
-# 		return
+	party_account_currency = get_account_currency(args.get("party_account"))
+	company_currency = frappe.get_cached_value('Company',  args.get("company"),  "default_currency")
 
-# 	args['party_account'] =  frappe.db.get_value('Account', {'account_type': 'Payable','is_group': 0, 'company': args.get('company')})
-# 	print(args)
+	# Get negative outstanding sales /purchase invoices
+	negative_outstanding_invoices = []
+	if args.get("party_type") not in ["Student", "Employee"] and not args.get("voucher_no"):
+		negative_outstanding_invoices = get_negative_outstanding_invoices(args.get("party_type"), args.get("party"),
+			args.get("party_account"), args.get("company"), party_account_currency, company_currency)
 
-# 	# confirm that Supplier is not blocked
-# 	if args.get('party_type') == 'Supplier':
-# 		supplier_status = get_supplier_block_status(args['party'])
-# 		if supplier_status['on_hold']:
-# 			if supplier_status['hold_type'] == 'All':
-# 				return []
-# 			elif supplier_status['hold_type'] == 'Payments':
-# 				if not supplier_status['release_date'] or getdate(nowdate()) <= supplier_status['release_date']:
-# 					return []
+	# Get positive outstanding sales /purchase invoices/ Fees
+	condition = ""
+	if args.get("voucher_type") and args.get("voucher_no"):
+		condition = " and voucher_type={0} and voucher_no={1}"\
+			.format(frappe.db.escape(args["voucher_type"]), frappe.db.escape(args["voucher_no"]))
 
-# 	party_account_currency = get_account_currency(args.get("party_account"))
-# 	company_currency = frappe.get_cached_value('Company',  args.get("company"),  "default_currency")
+	# Add cost center condition
+	if args.get("cost_center"):
+		condition += " and cost_center='%s'" % args.get("cost_center")
 
-# 	# Get negative outstanding sales /purchase invoices
-# 	negative_outstanding_invoices = []
-# 	if args.get("party_type") not in ["Student", "Employee"] and not args.get("voucher_no"):
-# 		negative_outstanding_invoices = get_negative_outstanding_invoices(args.get("party_type"), args.get("party"),
-# 			args.get("party_account"), args.get("company"), party_account_currency, company_currency)
+	date_fields_dict = {
+		'posting_date': ['from_posting_date', 'to_posting_date'],
+		'due_date': ['from_due_date', 'to_due_date']
+	}
 
-# 	# Get positive outstanding sales /purchase invoices/ Fees
-# 	condition = ""
-# 	if args.get("voucher_type") and args.get("voucher_no"):
-# 		condition = " and voucher_type={0} and voucher_no={1}"\
-# 			.format(frappe.db.escape(args["voucher_type"]), frappe.db.escape(args["voucher_no"]))
+	for fieldname, date_fields in date_fields_dict.items():
+		if args.get(date_fields[0]) and args.get(date_fields[1]):
+			condition += " and {0} between '{1}' and '{2}'".format(fieldname,
+				args.get(date_fields[0]), args.get(date_fields[1]))
 
-# 	# Add cost center condition
-# 	if args.get("cost_center"):
-# 		condition += " and cost_center='%s'" % args.get("cost_center")
+	if args.get("company"):
+		condition += " and company = {0}".format(frappe.db.escape(args.get("company")))
 
-# 	date_fields_dict = {
-# 		'posting_date': ['from_posting_date', 'to_posting_date'],
-# 		'due_date': ['from_due_date', 'to_due_date']
-# 	}
+	outstanding_invoices = get_outstanding_invoices(args.get("party_type"), args.get("party"),
+		args.get("party_account"), filters=args, condition=condition)
 
-# 	for fieldname, date_fields in date_fields_dict.items():
-# 		if args.get(date_fields[0]) and args.get(date_fields[1]):
-# 			condition += " and {0} between '{1}' and '{2}'".format(fieldname,
-# 				args.get(date_fields[0]), args.get(date_fields[1]))
+	for d in outstanding_invoices:
+		d["exchange_rate"] = 1
+		if party_account_currency != company_currency:
+			if d.voucher_type in ("Sales Invoice", "Purchase Invoice", "Expense Claim"):
+				d["exchange_rate"] = frappe.db.get_value(d.voucher_type, d.voucher_no, "conversion_rate")
+			elif d.voucher_type == "Journal Entry":
+				d["exchange_rate"] = get_exchange_rate(
+					party_account_currency,	company_currency, d.posting_date
+				)
+		if d.voucher_type in ("Purchase Invoice"):
+			d["bill_no"] = frappe.db.get_value(d.voucher_type, d.voucher_no, "bill_no")
 
-# 	if args.get("company"):
-# 		condition += " and company = {0}".format(frappe.db.escape(args.get("company")))
+	# Get all SO / PO which are not fully billed or aginst which full advance not paid
+	orders_to_be_billed = []
+	if (args.get("party_type") != "Student"):
+		orders_to_be_billed =  get_orders_to_be_billed(args.get("posting_date"),args.get("party_type"),
+			args.get("party"), args.get("company"), party_account_currency, company_currency, filters=args)
 
-# 	outstanding_invoices = get_outstanding_invoices(args.get("party_type"), args.get("party"),
-# 		args.get("party_account"), filters=args, condition=condition)
+	data = negative_outstanding_invoices + outstanding_invoices + orders_to_be_billed
+	if not data:
+		frappe.msgprint(_("No outstanding invoices found for the {0} {1} which qualify the filters you have specified.")
+			.format(args.get("party_type").lower(), frappe.bold(args.get("party"))))
 
-# 	for d in outstanding_invoices:
-# 		d["exchange_rate"] = 1
-# 		if party_account_currency != company_currency:
-# 			if d.voucher_type in ("Sales Invoice", "Purchase Invoice", "Expense Claim"):
-# 				d["exchange_rate"] = frappe.db.get_value(d.voucher_type, d.voucher_no, "conversion_rate")
-# 			elif d.voucher_type == "Journal Entry":
-# 				d["exchange_rate"] = get_exchange_rate(
-# 					party_account_currency,	company_currency, d.posting_date
-# 				)
-# 		if d.voucher_type in ("Purchase Invoice"):
-# 			d["bill_no"] = frappe.db.get_value(d.voucher_type, d.voucher_no, "bill_no")
-
-# 	# Get all SO / PO which are not fully billed or aginst which full advance not paid
-# 	orders_to_be_billed = []
-# 	if (args.get("party_type") != "Student"):
-# 		orders_to_be_billed =  get_orders_to_be_billed(args.get("posting_date"),args.get("party_type"),
-# 			args.get("party"), args.get("company"), party_account_currency, company_currency, filters=args)
-
-# 	data = negative_outstanding_invoices + outstanding_invoices + orders_to_be_billed
-# 	print(negative_outstanding_invoices, outstanding_invoices, orders_to_be_billed)
-# 	if not data:
-# 		frappe.msgprint(_("No outstanding invoices found for the {0} {1} which qualify the filters you have specified.")
-# 			.format(args.get("party_type").lower(), frappe.bold(args.get("party"))))
-
-# 	return data
+	return data
