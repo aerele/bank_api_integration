08460c7f55f278347c98368a9050aec0155398df
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-04-27 12:34:05 +0530

    fetch balance api call

--- bank_api_integration/bank_api_integration/doctype/bank_api_integration/bank_api_integration.py
+++ bank_api_integration/bank_api_integration/doctype/bank_api_integration/bank_api_integration.py
@@ -3,8 +3,53 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+import banking_api
+from banking_api.common_provider import CommonProvider
 
 class BankAPIIntegration(Document):
 	pass
+
+@frappe.whitelist()
+def fetch_balance(doc_name):
+	prov, config = get_api_provider_class(doc_name)
+	balance = 0
+	try:
+		res = prov.fetch_balance()
+		if res["RESPONSE"] == "SUCCESS":
+			balance = res['EFFECTIVEBAL']
+	except:
+		res = frappe.get_traceback()
+	log_name = log_request(doc_name,'Fetch Balance', config, res)
+	return balance
+
+def get_api_provider_class(doc_name):
+	integration_doc = frappe.get_doc('Bank API Integration', doc_name)
+	proxies = frappe.get_site_config().proxies
+	config = {"APIKEY": integration_doc.get_password(fieldname="api_key"), 
+			"CORPID": integration_doc.corp_id,
+			"USERID": integration_doc.user_id,
+			"AGGRID":integration_doc.aggr_id,
+			"AGGRNAME":integration_doc.aggr_name,
+			"URN": integration_doc.urn}
+	
+	file_paths = {'private_key': integration_doc.get_password(fieldname="private_key_path"),
+		'public_key': frappe.local.site_path + integration_doc.public_key_attachment}
+	
+	prov = CommonProvider(integration_doc.bank_api_provider, config, integration_doc.use_sandbox, proxies, file_paths, frappe.local.site_path)
+	return prov, config
+
+def log_request(doc_name, api_method, config, res):
+	request_log = frappe.get_doc({
+		"doctype": "Bank API Request Log",
+		"user": frappe.session.user,
+		"reference_document":doc_name,
+		"api_method": api_method,
+		"filters": None,
+		"config_details": str(config),
+		"response": res
+	})
+	request_log.save(ignore_permissions=True)
+	frappe.db.commit()
+	return request_log.name
